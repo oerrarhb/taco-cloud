ad81d71577a5f91116d1d245f02f78302a95b437
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2020-02-09 11:45:17 +0100

    Developing OrderController. #Closes 2.

---
+++ oer-taco-cloud/src/main/java/com/othmane/oertacocloud/controllers/OrderController.java
@@ -0,0 +1,20 @@
+package com.othmane.oertacocloud.controllers;
+
+import com.othmane.oertacocloud.data.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping
+    public String orderForm(Model model) {
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
+}
